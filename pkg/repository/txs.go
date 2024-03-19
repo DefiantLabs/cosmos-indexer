419a2370a96f36b4bd6b30802f19ba59ab2f5153
Artem Bogomaz <artembogomaz@gmail.com>
2024-03-19 10:42:42 +0700

    tx-responses model

--- pkg/repository/txs.go
+++ pkg/repository/txs.go
@@ -3,8 +3,9 @@ package repository
 import (
 	"context"
 	"fmt"
-	pb "github.com/DefiantLabs/cosmos-indexer/proto"
 	"time"
+
+	"github.com/DefiantLabs/cosmos-indexer/db/models"
 
 	"github.com/DefiantLabs/cosmos-indexer/pkg/model"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -12,6 +13,7 @@ import (
 
 type Txs interface {
 	ChartTxByDay(ctx context.Context, from time.Time, to time.Time) ([]*model.TxsByDay, error)
+	GetTxByHash(ctx context.Context, txHash string) (*models.Tx, error)
 }
 
 type txs struct {
@@ -47,9 +49,47 @@ func (r *txs) ChartTxByDay(ctx context.Context, from time.Time, to time.Time) ([
 	return data, nil
 }
 
-func (r *txs) GetTxByHash(ctx context.Context, txHash string) (*pb.TxByHashResponse, error) {
-	query := `select id, hash, code, block_id, timestamp from txes where hash=$1`
-	_ = r.db.QueryRow(ctx, query, txHash)
+func (r *txs) GetTxByHash(ctx context.Context, txHash string) (*models.Tx, error) {
+	query := `
+	   select txes.id as tx_id,
+       hash,
+       txes.code as tx_code,
+       block_id,
+       timestamp,
+       memo,
+       timeout_height,extension_options,
+       non_critical_extension_options,auth_info_id,tx_response_id,
+       auf.gas_limit, auf.payer, auf.granter, tip.tipper,
+       resp.code as tx_resp_code, resp.gas_used as tx_res_gas_used,
+       resp.gas_wanted as tx_res_gas_wanted, resp.raw_log
+	from txes
+		 left join tx_auth_info au on auth_info_id = au.id
+		 left join tx_auth_info_fee auf on au.fee_id = auf.id
+		 left join tx_tip tip on au.tip_id = tip.id
+		 left join tx_responses resp on tx_response_id = resp.id
+       where hash=$1`
 
-	return nil, nil
+	var tx models.Tx
+	var authInfo models.AuthInfo
+	var authInfoFee models.AuthInfoFee
+	var authInfoTip models.Tip
+	var txResponse models.TxResponse
+
+	row := r.db.QueryRow(ctx, query, txHash)
+	if err := row.Scan(&tx.ID, &tx.Hash, &tx.Code,
+		&tx.BlockID, &tx.Timestamp, &tx.Memo, &tx.TimeoutHeight,
+		&tx.ExtensionOptions, &tx.NonCriticalExtensionOptions,
+		&tx.AuthInfoID, &tx.TxResponseID,
+		&authInfoFee.GasLimit, &authInfoFee.Payer,
+		&authInfoFee.Granter, &authInfoTip.Tipper,
+		&txResponse.Code, &txResponse.GasUsed, &txResponse.GasWanted, &txResponse.RawLog); err != nil {
+		return nil, err
+	}
+
+	authInfo.Fee = authInfoFee
+	authInfo.Tip = authInfoTip
+	tx.AuthInfo = authInfo
+	tx.TxResponse = txResponse
+
+	return &tx, nil
 }
--- pkg/service/tx.go
+++ pkg/service/tx.go
@@ -2,9 +2,12 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"github.com/DefiantLabs/cosmos-indexer/db/models"
 	"github.com/DefiantLabs/cosmos-indexer/pkg/model"
 	"github.com/DefiantLabs/cosmos-indexer/pkg/repository"
 	pb "github.com/DefiantLabs/cosmos-indexer/proto"
+	"strings"
 	"time"
 )
 
@@ -26,5 +29,59 @@ func (s *txs) ChartTxByDay(ctx context.Context, from time.Time, to time.Time) ([
 }
 
 func (s *txs) GetTxByHash(ctx context.Context, txHash string) (*pb.TxByHashResponse, error) {
-	return nil, nil
+	tx, err := s.txRepo.GetTxByHash(ctx, strings.ToUpper(txHash))
+	if err != nil {
+		return &pb.TxByHashResponse{}, err
+	}
+
+	return s.txToProto(tx), nil
+}
+
+func (s *txs) txToProto(tx *models.Tx) *pb.TxByHashResponse {
+	return &pb.TxByHashResponse{
+		Tx: &pb.TxByHash{
+			Memo:                        tx.Memo,
+			TimeoutHeight:               fmt.Sprintf("%d", tx.TimeoutHeight),
+			ExtensionOptions:            tx.ExtensionOptions,
+			NonCriticalExtensionOptions: tx.NonCriticalExtensionOptions,
+			AuthInfo: &pb.TxAuthInfo{
+				PublicKey:  []string{}, // TODO
+				Signatures: []string{}, // TODO
+				Fee: &pb.TxFee{
+					Granter:  tx.AuthInfo.Fee.Granter,
+					Payer:    tx.AuthInfo.Fee.Payer,
+					GasLimit: fmt.Sprintf("%d", tx.AuthInfo.Fee.GasLimit),
+					Amount:   nil, // TODO
+				},
+				Tip: &pb.TxTip{
+					Tipper: tx.AuthInfo.Tip.Tipper,
+					Amount: s.txTipToProto(tx.AuthInfo.Tip.Amount),
+				},
+			},
+			TxResponse: &pb.TxResponse{
+				Height:    tx.TxResponse.Height,
+				Txhash:    tx.Hash,
+				Codespace: "", // TODO
+				Code:      int32(tx.TxResponse.Code),
+				Data:      "", // TODO
+				RawLog:    tx.TxResponse.RawLog,
+				Info:      "",  // TODO
+				Logs:      nil, // TODO
+				GasWanted: fmt.Sprintf("%d", tx.TxResponse.GasWanted),
+				GasUsed:   fmt.Sprintf("%d", tx.TxResponse.GasUsed),
+				Timestamp: tx.TxResponse.TimeStamp,
+			},
+		},
+	}
+}
+
+func (s *txs) txTipToProto(tips []models.TipAmount) []*pb.Denom {
+	denoms := make([]*pb.Denom, 0)
+	for _, tip := range tips {
+		denoms = append(denoms, &pb.Denom{
+			Denom:  tip.Denom,
+			Amount: tip.Amount.String(),
+		})
+	}
+	return denoms
 }
