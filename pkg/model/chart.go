5d7aa7ad47eaced86493a167a76bba1583fc3ecd
Artem Bogomaz <artembogomaz@gmail.com>
2024-06-14 11:47:28 +0700

    adding charts repo

--- pkg/model/chart.go
+++ pkg/model/chart.go
@@ -1,8 +1,27 @@
 package model
 
-import "time"
+import (
+	"github.com/shopspring/decimal"
+	"time"
+)
 
 type TxsByDay struct {
 	TxNum int32
 	Day   time.Time
+}
+
+type TxsByHour struct {
+	TxNum int32
+	Hour  time.Time
+}
+
+type TxByHourWithCount struct {
+	Points   []*TxsByHour
+	Total24H int64
+	Total48H int64
+}
+
+type TxVolumeByHour struct {
+	TxVolume decimal.Decimal
+	Hour     time.Time
 }
