6120f91116e9efa7330b9bb4ef435e235ae6dc48
Artem Bogomaz <a.bogomaz@emcd.io>
2024-06-19 17:13:49 +0700

    removing chainID

--- pkg/service/blocks.go
+++ pkg/service/blocks.go
@@ -9,9 +9,9 @@ import (
 )
 
 type Blocks interface {
-	BlockInfo(ctx context.Context, block int32, chainID int32) (*model.BlockInfo, error)
+	BlockInfo(ctx context.Context, block int32) (*model.BlockInfo, error)
 	BlockInfoByHash(ctx context.Context, hash string) (*model.BlockInfo, error)
-	BlockValidators(ctx context.Context, block int32, chainID int32) ([]string, error)
+	BlockValidators(ctx context.Context, block int32) ([]string, error)
 	TotalBlocks(ctx context.Context, to time.Time) (*model.TotalBlocks, error)
 	Blocks(ctx context.Context, limit int64, offset int64) ([]*model.BlockInfo, int64, error)
 	BlockSignatures(ctx context.Context, height int64, limit int64, offset int64) ([]*model.BlockSigners, int64, error)
@@ -25,16 +25,16 @@ func NewBlocks(blocksRepo repository.Blocks) *blocks {
 	return &blocks{blocksRepo: blocksRepo}
 }
 
-func (s *blocks) BlockInfo(ctx context.Context, block int32, chainID int32) (*model.BlockInfo, error) {
-	return s.blocksRepo.GetBlockInfo(ctx, block, chainID)
+func (s *blocks) BlockInfo(ctx context.Context, block int32) (*model.BlockInfo, error) {
+	return s.blocksRepo.GetBlockInfo(ctx, block)
 }
 
 func (s *blocks) BlockInfoByHash(ctx context.Context, hash string) (*model.BlockInfo, error) {
 	return s.blocksRepo.GetBlockInfoByHash(ctx, hash)
 }
 
-func (s *blocks) BlockValidators(ctx context.Context, block int32, chainID int32) ([]string, error) {
-	return s.blocksRepo.GetBlockValidators(ctx, block, chainID)
+func (s *blocks) BlockValidators(ctx context.Context, block int32) ([]string, error) {
+	return s.blocksRepo.GetBlockValidators(ctx, block)
 }
 
 func (s *blocks) TotalBlocks(ctx context.Context, to time.Time) (*model.TotalBlocks, error) {
